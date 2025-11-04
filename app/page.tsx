c1323cc20fe90b95adef03b4ae06cb0df255f04a
engaweis <maxamedaweys90@gmail.com>
2025-11-04 11:18:32 +0300

    fix paths some styling

--- app/page.tsx
+++ app/page.tsx
@@ -42,7 +42,7 @@ export default function Page() {
             src="/hero-mobile.png"
             width={560}
             height={620}
-            className='blockmd:hidden'
+            className='block md:hidden'
             alt="Screenshot of the application dashboard, showing mobile version."
             />
         </div>
